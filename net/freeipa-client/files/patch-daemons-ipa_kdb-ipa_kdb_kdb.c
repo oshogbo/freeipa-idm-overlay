2bbd5d9b79442f7f9ff7612ce4008e6d64375ad0
Mariusz Zaborski <mariusz@zaborski.pl>
2025-07-11 12:37:05 +0200

    Create __BSD_VISIBLE to define u_int from sys/types.h

--- net/freeipa-client/files/patch-daemons-ipa_kdb-ipa_kdb_kdb.c
+++ net/freeipa-client/files/patch-daemons-ipa_kdb-ipa_kdb_kdb.c
@@ -1,9 +1,13 @@
 --- daemons/ipa-kdb/ipa_kdb.c    2023-08-21 14:29:11.264700960 +0000
 +++ daemons/ipa-kdb/ipa_kdb.c        2024-06-14 10:29:21.726567000 +0000
-@@ -20,8 +20,10 @@
+@@ -20,8 +20,14 @@
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
  
++#define __BSD_VISIBLE 1
++#include <sys/types.h>
++#undef __BSD_VISIBLE
++
 +#include <sys/time.h>
  #include <talloc.h>
  #include <sys/utsname.h>
